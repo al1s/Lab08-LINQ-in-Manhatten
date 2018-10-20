be9ac0c55fd04228cd0a3cc1471aeaee7b746844
alstof <alstof@gmail.com>
2018-10-19 17:19:32 -0700

    Add LINQ queries to get neighborhoods

---
+++ Lab08-Linq/Classes/Geometry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Lab08_Linq.Classes
+{
+    public class Geometry
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("coordinates")]
+        public double[] Coordinates { get; set; }
+
+    }
+}
